e1e0fa3fe4b1d4fcaa7fa5d7463ae83b9e533fab
MichalStrouhal <278398@vutbr.cz>
2025-10-01 14:53:00 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+Last login: Wed Oct  1 14:35:11 on ttys000
+michalstrouhal@Michal-MacBook-Pro ~ % nano app.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  UW PICO 5.09                      File: app.py                      Modified  
+
+                
+        buffer.seek(0)  
+        st.success("PDF připraveno ke stažení.")
+        st.download_button("Stáhnout PDF", data=buffer, file_name="kruznice_rep$
+            
+st.write("---")
+st.caption("Tip: pro tisk do fyzické tiskárny můžeš stáhnout PDF a použít tisk $
+                
+st.write("## Další možnosti / nápady")
+st.write(   
+    """         
+    - Přidat měření délky oblouku mezi dvěma body.\n
+    - Přidat označení indexů bodů přímo v grafu.\n
+    - Export do SVG nebo EPS pro vektorové zpracování.
+    """     
+)
+
+                
+
+
+^G Get Help  ^O WriteOut  ^R Read File ^Y Prev Pg   ^K Cut Text  ^C Cur Pos   
+^X Exit      ^J Justify   ^W Where is  ^V Next Pg   ^U UnCut Text^T To Spell  
